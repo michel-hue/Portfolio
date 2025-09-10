e7f055acccc7cf8c40de081d69c4cee2d0c96fc1
michel-hue <desirekoffi2006@gmail.com>
2025-09-10 17:05:56 +0000

    ajout

--- dev-project/nuxt.config.ts
+++ dev-project/nuxt.config.ts
@@ -1,14 +1,14 @@
 export default defineNuxtConfig({
   app: {
     head: {
-       charset: 'utf-16',
+      charset: 'utf-16',
       viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
-      title: 'Portfolio', // default fallback title
+      title: 'Portfolio',
       htmlAttrs: {
         lang: 'fr',
       },
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       ]
     }
   }
